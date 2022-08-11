3b5acfcb980c82e028b1facdc38ff55bb54ea284
Support <Support@DESKTOP-7DS253L>
2022-08-11 22:05:04 +0100

    Add project files.

---
+++ SRPAndOCP/Principles.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPAndOCP
+{
+    /// <summary>
+    /// Application of SRP
+    /// The pharmacySubFee class only takes care of charges for both customers and hospitals
+    /// </summary>
+    public class PharmacySubFee : Hospital
+     {                                                          
+        public double HospitalCharge { get;} = 3000;
+        public double CustomerCharge { get;} = 4000;
+        public double HospitalDiscount { get; } = 0.3;
+        public double CustomerDiscount { get; } = 0.2;
+
+    }
+
+
+    /// <summary>
+    /// Customers inherit PharmacySub fee and can implement objects of both Pharmacy and hospital since Pharmacy also inherits Hospital
+    /// </summary>
+    public class Customer : PharmacySubFee
+    {
+        public string CustomerName { get; set; }
+        public double CustomerRegFee { get; set; }
+        public void GenerateFee()
+        {
+
+            try
+            {
+                double referenceFee;
+                double basicFee = CustomerCharge + ReferenceCharge;
+
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine(error.Message);
+            }
+            
+
+        }
+    }
+
+    /// <summary>
+    /// Application of Open/Closed Principle; The hospital class extends the Discount class without modifying it.
+    /// </summary>
+    /// <returns></returns>
+    public class Hospital:Discount
+    {
+        public string HospitalName { get; set; }
+        public double ReferenceCharge { get; } = 2000;
+
+        public double HospitalRegFee { get; set; }
+
+
+
+        /// <summary>
+        /// to calculate class for each class of user
+        /// </summary>
+        /// <returns></returns>
+        public override double GenerateDiscounta()
+        {
+            PharmacySubFee myPharmFee = new PharmacySubFee();
+            double hospitalFee;
+            hospitalFee = myPharmFee.HospitalCharge - (myPharmFee.HospitalDiscount * myPharmFee.HospitalCharge);
+            return hospitalFee;
+
+        }
+        public override double GenerateDiscountb()
+        {
+            PharmacySubFee myPharmFee = new PharmacySubFee();
+            double customerFee;
+            customerFee = myPharmFee.CustomerCharge - (myPharmFee.CustomerDiscount * myPharmFee.CustomerCharge);
+            return customerFee;
+
+        }
+
+        }
+
+
+
+
+
+    /// <summary>
+    /// to calculate discount for hospital and customer
+    /// </summary>
+    public abstract class Discount 
+
+    {
+        public abstract double GenerateDiscounta ();
+        public abstract double GenerateDiscountb();
+    }
+}
---
+++ SRPAndOCP/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SRPAndOCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome on board");
+            Console.WriteLine("Select Hospital-1,Customer-2");
+            string choice = Console.ReadLine();
+            choice.ToLower();
+
+
+
+            if (choice == "hospital")    // condition for type of uer; hospital or customer
+            {
+                Hospital hospital = new Hospital();
+                Console.WriteLine("Enter Hospital Name: ");
+                hospital.HospitalName = Console.ReadLine();
+                string firstHosp = hospital.HospitalName;
+                Console.WriteLine("Your discounted fee is:" + hospital.GenerateDiscounta());
+            }
+            else if (choice == "customer")
+            {
+                Customer customer = new Customer();
+                Console.WriteLine("Enter Customer Name: ");
+                customer.CustomerName = Console.ReadLine();
+                string firstcust = customer.CustomerName;
+                Console.WriteLine("Your discounted fee is: " + customer.GenerateDiscountb());
+            }
+
+           
+
+         }
+
+
+
+            
+          
+       
+    }
+}
